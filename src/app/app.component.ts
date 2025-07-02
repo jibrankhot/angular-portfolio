e3acb172d4d58d8c3cb324040f83354cb84ac544
jibran <jibrankhot5@gmail.com>
2025-07-02 16:35:47 +0530

    animation fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-// app.component.ts
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 // @ts-ignore
 import * as AOS from 'aos';
 
@@ -9,15 +10,28 @@ import * as AOS from 'aos';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  constructor(private router: Router, private viewportScroller: ViewportScroller) { }
+
   ngOnInit(): void {
     AOS.init({
       duration: 1200,
       easing: 'ease-in-out',
-      once: true, // only animate once
+      once: true, // Animate only once
+    });
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Scroll to top on route change
+        this.viewportScroller.scrollToPosition([0, 0]);
+
+        // Refresh AOS to re-trigger animations correctly
+        setTimeout(() => AOS.refresh(), 100);
+      }
     });
   }
-  ngAfterViewInit(): void {
-    setTimeout(() => AOS.refresh(), 500); // Ensures DOM is fully rendered before AOS fires
-  }
 
+  ngAfterViewInit(): void {
+    // Safety net refresh
+    setTimeout(() => AOS.refresh(), 500);
+  }
 }
