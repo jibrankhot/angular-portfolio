c7f36d15a4fafc973e72ed8bd903886b30ae1c2f
jibran <jibrankhot5@gmail.com>
2025-07-02 20:20:25 +0530

    .

--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact.component';
-<<<<<<< HEAD
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms';
-=======
->>>>>>> 2ef88f065ed590af64af0c318d4992228e1a0084
-
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
     ContactComponent,
     ContactFormComponent
   ],
@@ -22,13 +17,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     ContactRoutingModule,
     SharedModule,
     ReactiveFormsModule
-=======
-    ContactComponent
-  ],
-  imports: [
-    CommonModule,
-    ContactRoutingModule
->>>>>>> 2ef88f065ed590af64af0c318d4992228e1a0084
   ]
 })
 export class ContactModule { }
