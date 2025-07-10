06d6348429eee016c8b348156acb61d7ce37eddf
jibran <jibrankhot5@gmail.com>
2025-07-10 20:40:08 +0530

    project section dialouge updated

--- src/app/modules/projects/components/project-card/project-card.component.ts
+++ src/app/modules/projects/components/project-card/project-card.component.ts
@@ -14,6 +14,9 @@ export class ProjectCardComponent implements OnInit {
   activeIndex: number = 0;
   displayFullImage: boolean = false;
   currentFullImageSrc: string = '';
+  zoomed: boolean = false;
+
+  touchStartX = 0;
 
   ngOnInit(): void {
     this.galleriaImages = this.project.images.map((img: string) => ({
@@ -26,14 +29,68 @@ export class ProjectCardComponent implements OnInit {
 
   prevImage() {
     this.activeIndex = (this.activeIndex - 1 + this.galleriaImages.length) % this.galleriaImages.length;
+    this.updateCurrentImage();
   }
 
   nextImage() {
     this.activeIndex = (this.activeIndex + 1) % this.galleriaImages.length;
+    this.updateCurrentImage();
   }
 
   openFullImage(src: string) {
-    this.currentFullImageSrc = src;
-    this.displayFullImage = true;
+    const index = this.galleriaImages.findIndex(img => img.previewImageSrc === src);
+    if (index !== -1) {
+      this.activeIndex = index;
+      this.currentFullImageSrc = src;
+      this.displayFullImage = true;
+      this.zoomed = false;
+
+      setTimeout(() => {
+        const wrapper = document.querySelector('.dialog-wrapper') as HTMLElement;
+        wrapper?.focus();
+      }, 50);
+    }
+  }
+
+  toggleZoom() {
+    this.zoomed = !this.zoomed;
+  }
+
+  updateCurrentImage() {
+    const img = this.galleriaImages[this.activeIndex];
+    if (img) {
+      this.currentFullImageSrc = img.previewImageSrc;
+    }
+  }
+
+  handleKeyboard(event: KeyboardEvent) {
+    if (!this.displayFullImage) return;
+
+    switch (event.key) {
+      case 'ArrowRight':
+        this.nextImage();
+        event.preventDefault();
+        break;
+      case 'ArrowLeft':
+        this.prevImage();
+        event.preventDefault();
+        break;
+      case 'Escape':
+        this.displayFullImage = false;
+        event.preventDefault();
+        break;
+    }
+  }
+
+  onTouchStart(event: TouchEvent) {
+    this.touchStartX = event.changedTouches[0].screenX;
+  }
+
+  onTouchEnd(event: TouchEvent) {
+    const endX = event.changedTouches[0].screenX;
+    const delta = this.touchStartX - endX;
+    if (Math.abs(delta) > 50) {
+      delta > 0 ? this.nextImage() : this.prevImage();
+    }
   }
 }
